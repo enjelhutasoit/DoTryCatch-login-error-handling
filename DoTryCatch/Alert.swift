0d0132192119cadf0db48f2aff4f1bef9cbc9a3e
Enjel <enjel.hutasoit@gmail.com>
2020-03-08 10:03:56 +0700

    Create alert class

---
+++ DoTryCatch/Alert.swift
@@ -0,0 +1,21 @@
+//
+//  Alert.swift
+//  DoTryCatch
+//
+//  Created by Enjelina on 08/03/20.
+//  Copyright © 2020 enjelhutasoit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Alert {
+    
+    class func showBasic(title: String, message: String, vc: UIViewController) {
+        
+        let alert =  UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        vc.present(alert, animated: true)
+        
+    }
+}
